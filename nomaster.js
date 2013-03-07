5d4bdc197923d598340881650de91a4b5f68207d
Alex Mayfield <alexmax2742@gmail.com>
2013-03-06 22:29:49 -0500

    Document functions and object constructors.

--- nomaster.js
+++ nomaster.js
@@ -5,6 +5,9 @@ var util = require('util');
 var SERVER_CHALLENGE = 5560020;
 var LAUNCHER_CHALLENGE = 777123;
 
+/**
+ * Represents one Odamex server.
+ */
 var Server = function(address, port, max_server_age) {
 	this.address = address;
 	this.port = port;
@@ -13,19 +16,38 @@ var Server = function(address, port, max_server_age) {
 	this.timeoutId = setTimeout(this.timeout.bind(this), max_server_age);
 };
 util.inherits(Server, events.EventEmitter);
+
+/**
+ * Resets the timeout, without any additional information.
+ */
 Server.prototype.heartbeat = function() {
 	clearTimeout(this.timeoutId);
 	this.timeoutId = setTimeout(this.timeout.bind(this), this.max_server_age);
 };
+
+/**
+ * Resets the timeout, sets the verification bit on a server (which is used
+ * to decide if a Server should actually show up on the master), and populates
+ * the Server with additional information such as the server name and number of
+ * players.
+ */
 Server.prototype.serverinfo = function() {
 	this.heartbeat();
 	this.verified = true;
 	util.log("Server info not implemented");
 };
+
+/**
+ * Server has gone too long without a heartbeat or a full update.
+ */
 Server.prototype.timeout = function() {
 	this.emit('timeout');
 };
-Server.prototype.addressToBuffer = function() {
+
+/**
+ * Returns the address of the server packed into 4 bytes.
+ */
+ Server.prototype.addressToBuffer = function() {
 	var buffer = new Buffer(4);
 	var octets = this.address.split('.');
 	for (var i = 0;i < 4;i++) {
@@ -33,17 +55,36 @@ Server.prototype.addressToBuffer = function() {
 	}
 	return buffer;
 };
+
+/**
+ * Returns the port of the server packed as a 16-bit Little-Endian integer.
+ */
 Server.prototype.portToBuffer = function () {
 	var buffer = new Buffer(2);
 	buffer.writeInt16LE(this.port, 0);
 	return buffer;
 };
+
+/**
+ * Returns an IP address + port combined into one Buffer.
+ */
 Server.prototype.toBuffer = function() {
 	return Buffer.concat([this.addressToBuffer(), this.portToBuffer()]);
 };
+
+/**
+ * A datastructure that contains many Server objects.  The datastructure is
+ * organized in such a way that makes it easier to group the servers by IP
+ * address.  It also handles writing out a full server list message.
+ */
 var Servers = function() {
 	this.servers = {};
 };
+
+/**
+ * Adds or updates a server, depending on if the Server exists in the
+ * object or not.
+ */
 Servers.prototype.updateServer = function(address, port, max_server_age) {
 	if (!(address in this.servers)) {
 		this.servers[address] = {};
@@ -58,10 +99,24 @@ Servers.prototype.updateServer = function(address, port, max_server_age) {
 		util.log('Server sent heartbeat: ' + address + ':' + port);
 	}
 };
+
+/**
+ * Delete a Server.
+ */
 Servers.prototype.deleteServer = function(address, port) {
 	delete this.servers[address][port];
 	util.log('Remote server timed out: ' + address + ':' + port);
 };
+
+/**
+ * Returns a complete launcher challenge message containing a list of servers
+ * in a simple packed buffer format.
+ *
+ * The format is quite simple.  There is a 6 byte header containing a 4 byte
+ * magic number and a 2 byte count of the servers in the message.  Then, every
+ * individual server is written into the message using their IP+Port buffer
+ * representation.
+ */
 Servers.prototype.toBuffer = function () {
 	var serverBuffers = [];
 
@@ -83,6 +138,10 @@ Servers.prototype.toBuffer = function () {
 	return Buffer.concat(serverBuffers);
 };
 
+/**
+ * Master is the main application object.  It makes sense of any options,
+ * starts the appropriate servers, and stores the global Servers instance.
+ */
 var Master = function(options) {
 	this.options = {};
 	if (options) {
@@ -99,10 +158,24 @@ var Master = function(options) {
 	this.socket.on('message', this.message.bind(this));
 	this.socket.bind(this.options.PORT);
 };
+
+/**
+ * Default options.
+ * 
+ * MAX_SERVER_AGE is the amount of time in milliseconds that  a Server is
+ * allowed to live without a heartbeat or full update before it is
+ * automatically removed from the master.  PORT is simply the port number that
+ * the master listens on.
+ */
 Master.prototype.defaults = {
 	MAX_SERVER_AGE: 300000,
 	PORT: 15000
 };
+
+/**
+ * Handles incoming messages.  Can either be a challenge from the server, a
+ * full update from the server, or a challenge from the launcher.
+ */
 Master.prototype.message = function(msg, rinfo) {
 	var challenge = msg.readInt32LE(0);
 
